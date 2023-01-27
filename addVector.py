377a96cb8d00d787285d172947f74d5973c40172
4ommyx <nattawutth011@gmail.com>
2023-01-27 15:04:05 +0700

    27-1-23

---
+++ addVector.py
@@ -0,0 +1,10 @@
+def add_vactor(u,v):
+    total = []
+    if (len(u) or len(v) ) != 3:
+        return 'ERROR'
+    for i in range(len(u)):
+        t=u[i]+v[i]
+        total.append(t)
+    return total
+print(add_vactor([0.5,10.5,0.5],[0.25,0.5,10.0]))
+    
